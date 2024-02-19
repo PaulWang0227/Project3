4c2223d2aed089794e5a23ad8fa6ece2caf0ded8
PaulWang0227 <yifanwang040227@gmail.com>
2024-02-19 15:57:00 -0800

    change in OSMTest

--- proj3/testsrc/OpenStreetMapTest.cpp
+++ proj3/testsrc/OpenStreetMapTest.cpp
@@ -2,3 +2,14 @@
 #include "OpenStreetMap.h"
 #include "StringDataSource.h"
 #include "XMLReader.h"
+
+TEST(OpenStreetMap, SimpleExampleTest){
+    auto InStream = std::make_shared<CStringDataSource>(
+      "<?xml version='1.0' encoding='UTF-8'?>"
+        "<osm version=\"0.6\" generator=\"osmconvert 0.8.5\">"
+        "<node id=\"1\" lat=\"38.5178523\" lon=\"-121.7712408\"/>"
+        "<node id=\"2\" lat=\"38.535052\" lon=\"-121.7408606\"/>"
+        "</osm>");
+    auto Reader = std::make_shared<XMLReader>(InStream);
+    COpenStreetMap StreetMap(Reader);
+}
